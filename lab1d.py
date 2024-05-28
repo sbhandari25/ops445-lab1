3f8df0eb717390192ddd8ebf4d966899182b2d00
Santosh <bhandarisantosh196@gmail.com>
2024-05-27 23:25:03 -0400

    added lab1d

---
+++ lab1d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Define integer objects
+x = 10
+y = 2
+z = 5
+
+# Perform arithmetic operations and store the result in a variable
+result = x + y * z
+
+# Print the result with proper formatting
+print(f"{x} + {y} * {z} = {result}")
+
